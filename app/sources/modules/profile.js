00437a0517a660515b72a7b92c642994f91dd496
Shashwat Jolly <shashwat.jolly@gmail.com>
2017-10-12 01:09:14 +0530

    Added dummy options to profile.js, will add onPress functions later

--- app/sources/modules/profile.js
+++ app/sources/modules/profile.js
@@ -3,11 +3,15 @@ import {
   Text,
   View,
   TouchableOpacity,
+  TouchableNativeFeedback,
   Image,
+  Dimensions,
   Animated,
-  Easing
+  Easing,
+  ScrollView
 } from 'react-native';
 
+const window = Dimensions.get('window');
 
 export class Profile extends Component<{}> {
   constructor(props){
@@ -54,18 +58,38 @@ spring () {
   		inputRange: [0, 1],
   		outputRange: [500, 1]
   	});
-  	
     return (
       <View style={{flex:1}}>  
-        <View style={{flex:1,paddingTop:10,alignItems:'center',paddingBottom:200}}>
+        <View style={{flex:1,paddingTop:10,marginBottom: 38, alignItems:'center',paddingBottom:200}}>
           
           <Animated.Image
                 style={{width:120, height:120,opacity:1.0,margin:18, transform: [{scale: this.springValue}]}}
                 source = {require('../../../assets/icons/boy.png')}
           />
-          <Animated.Text style={{fontSize:18,fontWeight:'bold', transform: [{translateX}]}}> PRAVANDAN CHAND </Animated.Text>
+          <Animated.Text style={{fontSize:18,fontWeight:'bold', transform: [{translateX}]}}> SHASHWAT JOLLY </Animated.Text>
+          <Animated.Text style={{fontSize:18, transform: [{translateX}]}}> shashwat.jolly@gmail.com </Animated.Text>
           <Animated.Text style={{fontSize:16, transform: [{translateX: translateX1}]}}> 8750507753 </Animated.Text>
         </View>
+        <TouchableNativeFeedback>
+        	<View style={{flex:1,paddingBottom:38,marginTop: 16, backgroundColor:'#fc004f',alignItems:'center',paddingTop:16}}>
+            <Text style={{fontSize:16,color:'white',fontWeight:'bold'}}>My Wallet Balance</Text>
+            </View>
+        </TouchableNativeFeedback>
+        <TouchableNativeFeedback>
+        	<View style={{flex:1,paddingBottom:38,marginTop: 16, backgroundColor:'#fc004f',alignItems:'center',paddingTop:16}}>
+            <Text style={{fontSize:16,color:'white',fontWeight:'bold'}}>Help</Text>
+            </View>
+        </TouchableNativeFeedback>
+        <TouchableNativeFeedback>
+        	<View style={{flex:1,paddingBottom:38,marginTop: 16, backgroundColor:'#fc004f',alignItems:'center',paddingTop:16}}>
+            <Text style={{fontSize:16,color:'white',fontWeight:'bold'}}>About</Text>
+            </View>
+        </TouchableNativeFeedback>
+        <TouchableNativeFeedback>
+        	<View style={{flex:1,paddingBottom:38,marginTop: 16, backgroundColor:'#fc004f',alignItems:'center',paddingTop:16}}>
+            <Text style={{fontSize:16,color:'white',fontWeight:'bold'}}>Logout</Text>
+            </View>
+        </TouchableNativeFeedback>
       </View>
     );
   }
